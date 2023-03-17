a703a4b2064934c4beedf56fb7a71a60872ee08b
KusamaHub Team <107151820+kusamahubteam@users.noreply.github.com>
2023-03-17 17:21:36 +0700

    Update ecosystem.config.js
    Change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'KusamaHub Demo',
-        port: '4000',
+        port: '9000',
         exec_mode: 'cluster',
         instances: 'max',
         script: './.output/server/index.mjs'
